609d2cc5097bd04776abb2d2a27f7c06b7e32f38
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-04 06:30:55 +0100

    Initial commit.

---
+++ src/test/java/com/mrdiipo/profile/LoaningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
