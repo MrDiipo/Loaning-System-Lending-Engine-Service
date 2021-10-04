144ad95a62dc36880a6dec133f7e3af7df6b6c32
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-04 06:47:30 +0100

    Completed LoanRequest class.

--- src/main/java/com/mrdiipo/profile/domain/model/LoanRequest.java
+++ src/main/java/com/mrdiipo/profile/domain/model/LoanRequest.java
@@ -1,7 +1,28 @@
 package com.mrdiipo.profile.domain.model;
 
-public class LoanRequest {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.Duration;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public final class LoanRequest {
 
     private final int amount;
-    private final
+    private final User borrower;
+    private final Duration repaymentTerm;
+    private final double intrestRate;
+
+    public LoanRequest(int amount, User borrower, Duration repaymentTerm, double intrestRate) {
+        this.amount = amount;
+        this.borrower = borrower;
+        this.repaymentTerm = repaymentTerm;
+        this.intrestRate = intrestRate;
+    }
+
 }
